41a8e3b022695cab5843aa20105c880ed37252e4
KwonSW <yaho4752@gmail.com>
2018-05-08 13:25:55 +0900

    2nd test

---
+++ src/main/java/sv/test/main.java
@@ -0,0 +1,12 @@
+package OFFER;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		file k = new file();
+		k.CheckValidation("123456789");
+		k.Deposit(30000);
+	}
+
+}
