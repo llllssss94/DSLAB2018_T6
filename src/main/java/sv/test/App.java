4ba769953b5271d2300f1e0cb98b61038162dcc1
김상원 <llllssss94@gmail.com>
2018-05-08 11:52:12 +0900

    final test 2

--- src/main/java/sv/test/App.java
+++ src/main/java/sv/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!@#@sadsad#";
+        System.out.println( "Hello World!!@#@sads;
     }
 }
